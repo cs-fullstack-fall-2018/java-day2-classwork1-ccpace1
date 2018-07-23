0c2d8c2b184bb4c150452e1bacce6a19dc7cfae7
Christopher Pace <Christopher_pace@ymail.com>
2018-07-23 11:29:24 -0500

    Commit of Exercise11

---
+++ Exercise11.java
@@ -0,0 +1,17 @@
+public class Exercise11 {
+
+    public static void main(String[] args) {
+        System.out.println("Let's print numbers:");
+        int counter1 = 0;
+        while (counter1 <= 100) {
+            if (counter1%2==0){
+                System.out.println(counter1);
+                counter1++;}
+            else{
+                counter1++;
+                }
+
+
+        }
+    }
+}
